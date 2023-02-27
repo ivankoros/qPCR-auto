3cb27a6b379f17bfba9e630ea38f77866b222108
Ivan Korostenskij <ivan.koros2@gmail.com>
2023-02-27 16:28:17 -0500

    added tailwind styling, changed configs to run webpage for dynamic editing, and added prototype functionality for notification button. also added support for multiple file upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./templates/**/*.html",
-        "./static/src/**/*.js",
-        "./node_modules/flowbite/**/*.js"
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("flowbite/plugin")],
-}
-
+  content: [
+    "./templates/**/*.html",
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
