5a3b0de4f911d20e7d2b54dc8df01dfc57e53d10
Ivan Korostenskij <ivan.koros2@gmail.com>
2023-02-27 14:01:22 -0500

    added dynamic table which updates on-upload with file name and data of upload using javascript. added multi-file functionality

--- app.py
+++ app.py
@@ -12,49 +12,61 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 
-@app.route("/")
-@app.route("/index")
-def index():
-    return render_template("index.html")
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 class TableEntry(object):
     def __init__(self, name, time):
         self.name = name
         self.time = time
 
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'time': self.time
+        }
 
-@app.route('/', methods=['GET', 'POST'])
+# Define the route for the default URL, which loads the form
+@app.route('/')
+def default():
+
+    with open(os.path.join(app.config['UPLOAD_FOLDER'], 'data.json'), 'r') as f:
+        data = json.load(f)
+
+    uploads = [TableEntry(entry['name'], entry['time']) for entry in data]
+
+    return render_template('index.html', uploads=uploads)
+
+# Define the route for the upload page, which uploads the file after submitting the form
+@app.route('/process', methods=['GET', 'POST'])
+@app.route('/process', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']
-        file_name = secure_filename(file.filename)
+        files = request.files.getlist('file')
+        new_entries = []
 
-        # call the TableEntry class to create a new object
-        new_entry = TableEntry(file_name, datetime.datetime.now())
+        for file in files:
+            file_name = secure_filename(file.filename)
+            new_entry = TableEntry(file_name, str(datetime.datetime.now()))
+            new_entries.append(new_entry)
 
         with open("uploads/data.json") as f:
             data = json.load(f)
 
-        print(data)
+        for entry in new_entries:
+            data.append(entry.__dict__)
 
-        data.append(new_entry.__dict__)
-
-        with open(os.path.join(app.config['UPLOAD_FOLDER'], 'data.json'), 'w') as f:
+        with open("uploads/data.json", "w") as f:
             json.dump(data, f)
 
-        print("Upload successful for " + file_name)
+        print("Upload successful for " + ", ".join([entry.name for entry in new_entries]))
 
-    # with open(os.path.join(app.config['UPLOAD_FOLDER'], 'data.json'), 'r') as f:
-    #     data = json.load(f)
-    #
-    # uploads = [TableEntry(entry['name'], entry['time']) for entry in data]
+        with open("uploads/data.json", "r") as f:
+            data = json.load(f)
 
-    return render_template('index.html')  # uploads=uploads)
+        uploads = [TableEntry(entry['name'], entry['time']) for entry in data]
+        table_html = render_template('update_table.html', uploads=uploads)
+
+        return jsonify({'table_html': table_html})
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
