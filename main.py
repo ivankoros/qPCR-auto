66d6764cc0c3ab55beadf1b08056b9914d9985d7
ivankoros <ivan.koros2@gmail.com>
2023-03-17 23:23:52 -0400

    updated in-line comments for login form in main.py. adjusted padding for table to fit more columns on screen while remaining readable.

--- main.py
+++ main.py
@@ -122,8 +122,8 @@ def login():
     """
     form = LoginForm()  # Create an instance of the LoginForm class
 
-    if form.validate_on_submit():  # Change request.method == 'POST' to form.validate_on_submit()
-        username = form.username.data  # Access the username and password data from the form
+    if form.validate_on_submit():
+        username = form.username.data
         password = form.password.data
 
         user = User.query.filter_by(username=username).first()
