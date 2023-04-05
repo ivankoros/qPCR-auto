be1e98ea1c8757af518eaeb5a89af65b2823160c
ivankoros <ivan.koros2@gmail.com>
2023-04-05 11:40:40 -0400

    added docstring comments for reasoning for each test and what we expect it to do

--- tests/tests.py
+++ tests/tests.py
@@ -33,6 +33,12 @@ class FlaskAppTests(unittest.TestCase):
         db.drop_all()
 
     def test_login_successful(self):
+        """Test that a user can log in successfully (correct password and username)
+
+        This test is done to ensure that the login functionality works as expected
+         and allows users with valid credentials to access the application.
+
+        """
         response = self.app.post('/login',
                                  data=dict(username='testuser', password='testpassword'),
                                  follow_redirects=True)
@@ -40,6 +46,13 @@ class FlaskAppTests(unittest.TestCase):
         self.assertIn(b'testfile.txt', response.data)
 
     def test_login_failed(self):
+        """Test if a user can fail to log in (wrong password and username).
+
+        This test checks that the application denies access to users with
+        incorrect credentials and provides an appropriate error message,
+        maintaining the security of the application.
+
+        """
         response = self.app.post('/login',
                                  data=dict(username='wronguser', password='wrongpassword'),
                                  follow_redirects=True)
@@ -47,6 +60,12 @@ class FlaskAppTests(unittest.TestCase):
         self.assertIn(b'Invalid username', response.data)
 
     def test_register_successful(self):
+        """Test if a user can successfully register (valid registration key).
+
+        This test ensures that the registration process is functional and
+        allows new users to register when provided with a correct registration key.
+
+        """
         with patch.dict(os.environ, {'REGISTRATION_KEY': 'testkey'}):
             response = self.app.post('/register',
                                      data=dict(username='newuser',
@@ -57,6 +76,13 @@ class FlaskAppTests(unittest.TestCase):
         self.assertIn(b'User created successfully', response.data)
 
     def test_register_failed(self):
+        """Test if a user can fail to register (invalid registration key).
+
+        This test is done to verify that the application denies
+        registration to users with incorrect registration keys, thereby
+        ensuring that only authorized users can register.
+
+        """
         with patch.dict(os.environ, {'REGISTRATION_KEY': 'testkey'}):
             response = self.app.post('/register',
                                      data=dict(username='newuser',
@@ -67,10 +93,24 @@ class FlaskAppTests(unittest.TestCase):
         self.assertIn(b'Invalid registration key', response.data)
 
     def test_login_required(self):
+        """Test if the login_required decorator works as expected
+
+        This test confirms that the login_required decorator is effectively
+        protecting routes that require authentication, thereby maintaining access
+        control for specific parts of the application.
+
+        """
         response = self.app.get('/', follow_redirects=True)
         self.assertIn(b'Login', response.data)
 
     def test_upload_file(self):
+        """Test if the file upload route works correctly (we can go to it).
+
+        This test ensures that the file upload process is working
+        properly, enabling users to upload valid files, store them
+        in the database, and display the uploaded files on the home page.
+
+        """
         self.app.post('/login',
                       data=dict(username='testuser', password='testpassword'),
                       follow_redirects=True)
@@ -85,6 +125,12 @@ class FlaskAppTests(unittest.TestCase):
         self.assertIn(b'success', response.data)
 
     def test_validate_file_valid(self):
+        """Test if the file validation route returns valid when given a file with the expected dimensions and/or content.
+
+        This test verifies that the file validation route can correctly identify valid files based on their
+         dimensions, allowing for accurate processing of valid files in the application.
+        :return:
+        """
         with open('tests/testfiles/raw_counts.xlsx', 'rb') as file:
             response = self.app.post('/validate_file/96x16',
                                      data=dict(file=file),
@@ -93,12 +139,20 @@ class FlaskAppTests(unittest.TestCase):
         self.assertIn(b'true', response.data)
 
     def test_validate_file_invalid(self):
+        """Test if the file validation route returns invalid when given a file with incorrect dimensions and/or content.
+
+        This test ensures that the file validation route can identify invalid files based on their dimensions
+        and prevent them from being processed by the application, maintaining data integrity and avoiding potential
+        errors.
+
+        """
         with open('tests/testfiles/invalid_dimensions.xlsx' 'rb') as file:
             response = self.app.post('/validate_file/96x16',
                                      data=dict(file=file),
                                      content_type='multipart/form-data')
 
         self.assertIn(b'false', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
